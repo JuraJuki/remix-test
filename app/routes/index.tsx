5e5628708c00c73d277170d03c1d07ec68a041a6
JuraJuki <juraypavlovic@gmail.com>
2022-11-01 11:37:04 +0100

    git commit -styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ import type { LinksFunction } from "@remix-run/node";
 
 import stylesUrl from "~/styles/index.css";
 
-export const links: LinksFunction = () => {
+export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
